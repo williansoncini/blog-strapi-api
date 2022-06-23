72c5a61a785efb298b8694231be7cb51e5f7fb91
williansoncini <willian_soncini@hotmail.com>
2022-06-23 20:23:48 -0300

    Tentando deploy na heroku

--- config/database.js
+++ config/database.js
@@ -1,13 +1,32 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL'),
-    },
-  },
-});
+const parse = require('pg-connection-string').parse;
+    const config = parse(process.env.DATABASE_URL);
+    module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false
+          },
+        },
+        debug: false,
+      },
+    });
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST'),
+//       port: env.int('DATABASE_PORT'),
+//       database: env('DATABASE_NAME'),
+//       user: env('DATABASE_USERNAME'),
+//       password: env('DATABASE_PASSWORD'),
+//       ssl: env.bool('DATABASE_SSL'),
+//     },
+//   },
+// });
